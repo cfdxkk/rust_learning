fef6296459c3d681dbe4bd3482d2d5a5e1eb70ec
cfdxkk <b2567240058@gmail.com>
2024-05-16 08:00:56 +0000

    :sparkles: 猜数游戏

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    let target_number = rand::thread_rng().gen_range(1..101);
+
+    println!("target number is {}", target_number);
+
+    println!("猜数（1 - 100 之间）");
+
+    loop {
+        println!("请输入一个数并回车：");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("读取输入内容出错！");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("您输入的可能不是一个 u32 整数！");
+                continue;
+            },
+        };
+    
+
+        match guess.cmp(&target_number) {
+            Ordering::Less => println!("你猜的数字 {} 太小了！", guess),
+            Ordering::Greater => println!("你猜的数字 {} 太大了！", guess),
+            Ordering::Equal => {
+                println!("你猜对了！{} 是正确的数字！", guess);
+                break;
+            },
+        }
+    }
 }
