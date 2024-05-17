9a27b30ba1c89bfbed37831e69db310821129235
cfdxkk <b2567240058@gmail.com>
2024-05-17 08:22:02 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut str1 = String::from("Hello world!");
+    const STR2: &str = "foo bar";
+
+    let result1 = find_first_world(&str1[..]);
+    let result2 = find_first_world(STR2);
+
+    // str1.clear(); // ERROR
+
+    println!("空格前的单词是：{}", result1);
+    println!("空格前的单词是：{}", result2);
+}
+
+fn find_first_world(string: &str) -> &str {
+    let str_bytes_array = string.as_bytes();
+    for (index, &str_byte) in str_bytes_array.iter().enumerate() {
+        if str_byte == b' ' {
+            return &string[..index];
+        }
+    }
+    &string[..]
 }
