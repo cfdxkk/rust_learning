41e8bf60c521e865c6b9182a26289e798f32dba3
cfdxkk <b2567240058@gmail.com>
2024-05-21 08:13:33 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,18 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rectangle = Rectangle {
         width: 20,
         height: 10,
     };
 
-    let size = area(&rectangle);
+    let size = rectangle.area();
     println!("size is: {}", size);
-}
-
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
