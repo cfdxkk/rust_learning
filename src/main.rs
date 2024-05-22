04f0a45f325a69f3e82483285f4b8e10fa42c7d6
cfdxkk <b2567240058@gmail.com>
2024-05-22 06:24:37 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-enum ipAddress {
+#[derive(Debug)]
+enum IpAddress {
     V4(u8, u8, u8, u8),
     V6(String),
 }
 
 fn main() {
-    let ipv4 = ipAddress::V4;
-    let ipv6 = ipAddress::V6;
+    let ipv4 = IpAddress::V4;
+    let ipv6 = IpAddress::V6;
+
+    let v4 = ipv4(1, 2, 3, 4);
+    let v6 = ipv6(String::from("::1"));
+
+    println!("ipv4 is {:#?}", v4);
+    println!("ipv6 is {:#?}", v6);
 }
